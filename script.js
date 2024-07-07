34fbddde0c86c27313500df0b3658f589a2ec534
Suara Temilola Barakat <suaratemilolabarakat@gmail.com>
2024-07-07 10:08:17 +0100

    modifed js code

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
 const dateTime = () => {
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
   const currentDate = new Date();
-  const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
-  const UTCTime = currentDate.getTime();
 
-  document.querySelector('[data-testid="currentDay"]').textContent = dayOfWeek;
-  document.querySelector('[data-testid="currentTimeUTC"]').textContent =
-    UTCTime;
+  const formattedDate = currentDate.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+
+  document.querySelector('[data-testid="currentDay"]').textContent =
+    formattedDate;
 };
 
-// Update the date and time every second
 setInterval(dateTime, 1000);
 
-// Initial update
 dateTime();
